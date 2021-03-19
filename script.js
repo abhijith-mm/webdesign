5b74f836fda6df222d481459f1d0ee4507a20f94
abhijith <abhijithmadu@gmail.com>
2021-03-19 18:59:32 +0530

    new

--- script.js
+++ script.js
@@ -8,3 +8,54 @@ scrollBtn.addEventListener("click",()=>{
     document.body.scrollTop=0;
     document.documentElement.scrollTop=0;
 })
+
+
+
+// $(document).ready(function(){
+//     $("#submit-form").validate({
+//         rules:{
+//             name:{
+//             required:true,
+//             minlength:4,
+//             maxlength:15
+//         },
+//         phone:{
+//             required:true,
+//             number:true,
+//             minlength:10
+//         },
+//         mail:{
+//             required:true,
+//             email:true
+//         },
+//         content:{
+//             required:true,
+//             minlength:10
+            
+
+//         }
+
+//     },
+//     messages:{
+//         name:{
+//             required:"Enter the name"
+    
+//         },
+    
+//     phone:{
+//          required:"Enter the number"
+        
+//     },
+//     mail:{
+//         required:"Enter the mail"
+        
+//     },
+//     content:{
+        
+
+//     }
+// }
+
+//     })
+// })
+
